d75bfdc87acf184ce422a800bf0c202322c32d99
Ella Tanttu <ellajtanttu@gmail.com>
2022-04-15 15:54:17 -0700

    route connected to db

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,7 +1,34 @@
+import { LoaderFunction } from "@remix-run/node"
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server"
+
+type LoaderData = {
+  joke: Joke;
+}
+
+export const loader: LoaderFunction = async ({
+  params,
+}) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  })
+  if (!joke) throw new Error("Joke not found");
+  const data: LoaderData = { joke };
+  return json(data);
+}
+
 export default function JokesJokeId() {
+  const data = useLoaderData<LoaderData>();
   return (
     <div>
-      <p>This is a unique joke</p>
+
+
+      <p>{data.joke.content}</p>
+      <Link to="."> {data.joke.name} Permalink</Link>
+
+
     </div>
   )
 }
